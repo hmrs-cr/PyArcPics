45144cd87d125c85714c6fba25b5cf820b9d0fe5
hmrs-cr <ranametal@users.sf.net>
2015-04-17 21:25:22 -0600

    Added Google+ Photos Autobackup (Picasa) uploader

---
+++ gphotosuploader.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+# coding=UTF8
+import pyexiv2
+import os
+import Image
+import time
+
+import gdata.photos.service
+import gdata.media
+import gdata.geo
+import sys
+from pictureuploader import PictureUploader
+
+import utils
+
+
+class GoogleUploader(PictureUploader):
+    def __init__(self, email, password):
+        PictureUploader.__init__(self)
+        self._gd_client = gdata.photos.service.PhotosService()
+        self._gd_client.email = email  # Set your Picasaweb e-mail address...
+        self._gd_client.password = password  # ... and password
+        self._autobackup_album = None
+        self._set_service_name("gphotos")
+        self.original_size = False
+
+    def get_autobackup_album_url(self):
+        if self._autobackup_album is None:
+            self._autobackup_album = "/data/feed/api/user/default/albumid/default"
+            albums = self._gd_client.GetUserFeed()
+            for album in albums.entry:
+                if album.name.text == "InstantUpload":
+                    self._autobackup_album = "/data/feed/api/user/default/albumid/" + album.gphoto_id.text
+                    break
+
+        return self._autobackup_album
+
+    def resize_image(self, org_file_name, rez_file_name, max_width=2048):
+        img = Image.open(org_file_name)
+
+        w, h = img.size
+
+        if w <= max_width and h <= max_width:
+            return False
+
+        if w > h:
+            ratio = (max_width / float(w))
+            h = int((float(h) * float(ratio)))
+            w = max_width
+        elif w < h:
+            ratio = (max_width / float(h))
+            w = int((float(w) * float(ratio)))
+            h = max_width
+        else:
+            w = max_width
+            h = max_width
+
+        img = img.resize((w, h), Image.ANTIALIAS)
+        img.save(rez_file_name)
+
+        org_exif_data = pyexiv2.ImageMetadata(org_file_name)
+        org_exif_data.read()
+        rez_exif_data = pyexiv2.ImageMetadata(rez_file_name)
+        rez_exif_data.read()
+
+        org_exif_data.copy(rez_exif_data, True, True, True, True)
+        rez_exif_data.write()
+
+        datetime = utils.get_date_from_file_date(org_file_name)
+        filetime = time.mktime(datetime.timetuple())
+        os.utime(rez_file_name, (filetime, filetime))
+
+        return True
+
+    def upload_file(self, file_name, md5sum=None):
+        album_url = self.get_autobackup_album_url()
+        fname = os.path.basename(file_name)
+        temp_file_name = None
+        photo_id = 0
+        try:
+            if not self.original_size:
+                path, name = os.path.split(file_name)
+                temp_file_name = os.path.join(path, ".resizing___" + name)
+                if self.resize_image(file_name, temp_file_name):
+                    file_name = temp_file_name
+                else:
+                    temp_file_name = None
+
+            photo = self._gd_client.InsertPhotoSimple(album_url, fname, "", file_name)
+            photo_id = photo.gphoto_id.text
+            print photo_id
+        except Exception as e:
+            print "Failed to upload file", fname + ":", e
+
+        if temp_file_name is not None:
+            os.remove(temp_file_name)
+
+        return photo_id
+
+    def authenticate(self):
+        try:
+            self._gd_client.source = 'personal-uploader-hmsoft-com'
+            self._gd_client.ProgrammaticLogin()
+            return True
+        except Exception as e:
+            sys.stderr.write(str(e) + "\n")
+            return False
---
+++ main_gphotosuploader.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# coding=UTF8
+
+import argparse
+import os
+import sys
+
+from gphotosuploader import GoogleUploader
+import utils
+
+
+def main():
+    def scan(fup):
+        print "Scanning folder", options.folder, "..."
+        pc, npc, pcs, npcs = fup.scan_directory(options.folder)
+        print pc, "total pictures found. (" + utils.sizeof_fmt(pcs) + ")"
+        print npc, "other files found. (" + utils.sizeof_fmt(npcs) + ")"
+
+    parser = argparse.ArgumentParser(description='Upload to Google+ all JPEG pictures in the given folder recursively')
+    parser.add_argument('folder', help='The folder to search for pictures')
+    parser.add_argument('-s', dest='scan_only', action="store_true", help="Scan folder but don't upload pictures")
+    parser.add_argument('-u', dest="user_name",  help='Google account user name.', default=None)
+    parser.add_argument('-p', dest="password", help='Google account password.', default=None)
+    parser.add_argument('-r', dest="small_size", action="store_true", help='Reduce image size before upload.')
+
+    options = parser.parse_args()
+
+    if not options.folder:
+        parser.print_help()
+        exit()
+
+    if options.scan_only:
+        scan(GoogleUploader("", ""))
+        exit()
+
+    if not options.user_name:
+        options.user_name = raw_input("Please enter Google account user name: ")
+
+    if not options.password:
+        options.password = raw_input("Please enter Google account password: ")
+
+    gup = GoogleUploader(options.user_name, options.password)
+
+    print "Authenticating..."
+    if not gup.authenticate():
+        sys.stderr.write("Google authentication error\n")
+        exit()
+
+    print "Starting upload"
+    options.folder = unicode(options.folder, "UTF-8")
+    gup.original_size = not options.small_size
+
+    if os.path.isfile(options.folder):
+        gup.upload_file(options.folder)
+        print "Done."
+    else:
+        scan(gup)
+        t = gup.upload_directory(options.folder)
+        print "Done in " + utils.format_time(t)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print "Terminated by user."
